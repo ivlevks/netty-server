7aeaff47ba91d48dc333fc74714900e8d5235c10
ivlevks <ivlevks@yandex.ru>
2024-03-26 20:55:08 +0300

    init spring boot app

---
+++ src/main/java/ivlevks/simplenettyserver/SimpleNettyServerApplication.java
@@ -0,0 +1,13 @@
+package ivlevks.simplenettyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNettyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleNettyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ivlevks/simplenettyserver/SimpleNettyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ivlevks.simplenettyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNettyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
